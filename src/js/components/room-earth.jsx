d00f0f8b40ef476c77fe018e6b53667b22afa773
mynameislau <mynameislau@gmail.com>
2016-11-21 08:37:55 +0100

    lat long to three js formula

--- src/js/components/room-earth.jsx
+++ src/js/components/room-earth.jsx
@@ -9,28 +9,47 @@ const mapStateToProps = state => ({
   tweets: state.tweets
 });
 
-const lonLatToVector3 = ( lng, lat ) =>
-{    //flips the Y axis
-    lat = Math.PI / 2 - lat;
+// const lonLatToVector3 = ( lon, lat ) =>
+// {    //flips the Y axis
+//   const polar   = (90 - lat) * (Math.PI / 180);
+//   const azimuth = (180 - lon) * (Math.PI / 180);
+//   const radius = 1;
+//
+//   console.log(polar, azimuth, radius);
+//
+//     //distribute to sphere
+//     return [
+//       radius * Math.sin(polar) * Math.cos(azimuth),
+//       radius * Math.cos(polar)
+//       -radius * Math.sin(polar) * Math.sin(azimuth),
+//     ];
+//
+// }
 
-    //distribute to sphere
+/**
+	 * Converts a latlong to Vector3 for use in Three.js
+	 */
+	function lonLatToVector3 (lon, lat, radius = 1) {
+	  const phi = (90 - lat) * Math.PI / 180;
+	  const theta = (180 - lon) * Math.PI / 180;
+
     return [
-      Math.sin( lat ) * Math.sin( lng ),
-      Math.cos( lat ),
-      Math.sin( lat ) * Math.cos( lng )
-    ];
-
-}
+      Math.sin(phi) * Math.cos(theta) * radius,
+      Math.cos(phi) * radius,
+      -Math.sin(phi) * Math.sin(theta) * radius
+    ]
+	};
 
 const component = ({ tweets }) =>
   <Entity>
     <VisorCam/>
-    <Entity primitive="a-sky" material="src: url(assets/earth3_bw.png)"/>
+    <Entity primitive="a-sky" material="src: url(assets/earth3.jpg)"/>
     {
       tweets.map(tweet => {
-        const vec3D = lonLatToVector3(tweet[0], tweet[1]).map(pos => pos);
+        //console.log(tweet);
+        const vec3D = lonLatToVector3(tweet[0], tweet[1], 10).map(pos => pos);
         console.log(vec3D);
-        return <Entity geometry="primitive: box; width: 0.05; height: 0.05; depth: 0.05;" material="color: red" position={`${vec3D[0]} ${vec3D[1]} ${vec3D[2]}`}/>;
+        return <Entity geometry="primitive: sphere; radius: 0.05;" material="color: red; shader: flat" position={`${vec3D[0]} ${vec3D[1]} ${vec3D[2]}`}/>;
       })
     }
   </Entity>;
--- src/js/reducers/tweets.js
+++ src/js/reducers/tweets.js
@@ -1,7 +1,12 @@
 import { Map, List } from 'Immutable';
 import { ADD_TWEET } from '../actions/tweets';
 
-const defaultState = List();
+const defaultState = List([
+  [0, 45],
+  [0, 22],
+  [0, 0],
+  [-45, 0]
+]);
 
 export const tweetsReducer = (prevState = defaultState, action) => {
   const payload = action.payload;
@@ -11,7 +16,8 @@ export const tweetsReducer = (prevState = defaultState, action) => {
 
     case ADD_TWEET:
       if (payload.data.coordinates) {
-        return prevState.push(payload.data.coordinates.coordinates).takeLast(50);
+        console.log('tweet');
+        return prevState.push(payload.data.coordinates.coordinates).takeLast(200);
       }
 
     default:
